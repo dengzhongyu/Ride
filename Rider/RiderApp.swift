3cd9b13cde6a12499a650522927332a33b540a61
DZY <zhongyu_deng>
2024-11-16 07:38:04 +0800

    Initial Commit

---
+++ Rider/RiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiderApp.swift
+//  Rider
+//
+//  Created by zhongyu deng on 2024/11/16.
+//
+
+import SwiftUI
+
+@main
+struct RiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
